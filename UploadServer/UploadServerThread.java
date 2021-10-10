848c2b0ef728892707d360b0f857887cbe125784
Sam Shannon <sshannon237@gmail.com>
2021-10-10 09:41:48 -0700

    Changes for reading from bytearray

--- UploadServer/UploadServerThread.java
+++ UploadServer/UploadServerThread.java
@@ -14,19 +14,49 @@ public class UploadServerThread extends Thread {
    public void run() {
       try {
          // Creates a HttpServletRequest instance
-         InputStream in = socket.getInputStream(); 
+         InputStream in = socket.getInputStream();
          HttpServletRequest req = new HttpServletRequest(in);
 
          // Creates a HttpServletResponse instance
-         OutputStream baos = new ByteArrayOutputStream(); 
-         HttpServletResponse res = new HttpServletResponse(baos);
+         OutputStream output = new ByteArrayOutputStream();
+         HttpServletResponse res = new HttpServletResponse(output);
 
          // Creates a HttpServlet Instance
          HttpServlet httpServlet = new UploadServlet();
 
-         httpServlet.doPost(req, res);
+         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+         String inputLine;
+         inputLine = bufferedReader.readLine();
 
+         if (inputLine.contains("GET / ")) {
+            httpServlet.doGet(req, res);
+         } else if (inputLine.contains("POST")) {
+            httpServlet.doGet(req, res);
+         } else {
+            httpServlet.doGet(req, res);
+         }
+         // System.out.println();
+         // BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+         // String inputLine;
+      
+         // ByteArrayOutputStream baos = new ByteArrayOutputStream();  
+         // byte[] content = new byte[1];
+         // int bytesRead = -1;      
+         // while( ( bytesRead = in.read( content ) ) != -1 ) {  
+         //    baos.write( content, 0, bytesRead );  
+         // }
+         // inputLine = baos.toString();
+         // System.out.println(inputLine);
+         // if (inputLine.contains("GET / ")) {
+         //    httpServlet.doGet(req, res);
+         // } else if (inputLine.contains("POST")) {
+         //    httpServlet.doPost(req, res);
+         // } else {
+         //    httpServlet.doPost(req, res);
+         // }
 
-      } catch (Exception e) { e.printStackTrace(); }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
    }
 }
