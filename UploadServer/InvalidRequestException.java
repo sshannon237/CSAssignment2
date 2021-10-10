949055a4afcd4496ce4c09ebc7d7630325cb94cf
Chris Spooner <Christopher.Spooner97@gmail.com>
2021-10-10 11:09:44 -0700

    Create commented out basic implementation of the exception

--- UploadServer/InvalidRequestException.java
+++ UploadServer/InvalidRequestException.java
@@ -1,7 +1,12 @@
 package UploadServer;
 
 public class InvalidRequestException extends Exception {
-    public InvalidRequestException(String errorMessage) {
-        super(errorMessage);
+    public InvalidRequestException(String errorMessage, Throwable err) {
+        super(errorMessage, err);
     }
 }
+//try {
+//    //Code goes here
+//} catch (InvalidRequestException e){
+//        throw new InvalidRequestException("Invalid request", e);
+//}
