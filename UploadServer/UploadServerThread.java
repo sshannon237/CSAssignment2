24b45d0b001c0c60574c468e0aecf2c43311d98d
Chris Spooner <Christopher.Spooner97@gmail.com>
2021-10-10 17:48:51 -0700

    update storage of image size

--- UploadServer/UploadServerThread.java
+++ UploadServer/UploadServerThread.java
@@ -24,17 +24,29 @@ public class UploadServerThread extends Thread {
          // Creates a HttpServlet Instance
          HttpServlet httpServlet = new UploadServlet();
 
+         // Reads the Header from the input Stream and gets the image size as int
          BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
          String input = "";
          String inputLine = "";
+         int imageSize = 0;
          while(!(inputLine = bufferedReader.readLine()).equals("") ) {
             input += inputLine + "\n";
             if(inputLine.contains("Content-Length")){
                String[] contentLength = inputLine.split(" ", 2);
-               System.out.println(contentLength[1]);
+               imageSize = Integer.parseInt(contentLength[1]);
             }
          }
-         System.out.println(input);
+         System.out.println(imageSize);
+
+         //         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//         byte[] content = new byte[1];
+//         int bytesRead = -1;
+//         while( ( bytesRead = in.read( content ) ) != -1 ) {
+//            baos.write( content, 0, bytesRead );
+//         }
+//         String s = baos.toString();
+//         System.out.println(s);
+//         System.out.println(baos.toString());
 
          if (input.contains("GET / ")) {
             httpServlet.doGet(req, res);
